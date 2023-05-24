c1d4d2b0c80e6a4a70deb82abd6c4537c3aa64d2
Ewan Edwards <eskwayrd@gmail.com>
2023-05-24 10:58:13 -0700

    Add some debugging, and ensure that the page variable gets updated.

--- fix_redirect_tense.js
+++ fix_redirect_tense.js
@@ -11,13 +11,17 @@ const path  = require('path')
 const u     = require('./utils')
 const walk  = require('walk-sync')
 
+u.DEBUG = true
+
 const playbook = u.getPlaybook()
 if (!playbook) {
   u.log(chalk.red(`No playbook values!`))
   process.exit(1)
 }
+u.debug(`Playbook:`, playbook)
 
 const buildDir = playbook.output.dir || 'build'
+u.debug(`buildDir: ${buildDir}`)
 if (!fs.existsSync(buildDir)) {
   u.log(chalk.red(`Cannot open build dir ${buildDir}!`))
   process.exit(1)
@@ -30,17 +34,21 @@ const walkConfig = {
   ],
 }
 
+u.debug(`Walking ${buildDir}...`)
 walk(buildDir, walkConfig).map((fileRel) => {
   const filePath = path.join(buildDir, fileRel)
+  u.debug(`Considering ${filePath}...`)
   var page = fs.readFileSync(filePath, { encoding: 'utf8' })
   if (
     page.match(/<title>Redirect Notice<\/title>/)
     && page.match(/<h1>Redirect Notice<\/h1>/)
     && page.match(/The page you requested has been relocated/)
   ) {
-    page.replace('Redirect Notice', 'Redirect notice')
-    page.replace('requested has been relocated', 'requested has relocated')
+    u.debug(`It is a redirect page!`)
+    page = page.replace(/Redirect Notice/g, 'Redirect notice')
+    page = page.replace(/requested has been relocated/, 'requested has relocated')
 
+    u.debug(`Writing updating file: ${page}`)
     fs.writeFileSync(
       filePath,
       Buffer.from(page),
